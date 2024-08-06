793699e56fac8c9f6450640d181890faa71de290
weeezik <mkihanya.33@gmail.com>
2024-08-06 11:59:04 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# hangman
+# Hangman
+Building a command line Hangman game where one player plays against the computer.
+## General Specs
+ - word selection from text file
+ - GUI on command line
+ - receive player input (input reflects on GUI)
+ - save game functionality (and loading a saved game functionality)
--- main.rb
+++ main.rb
@@ -1,16 +1,18 @@
 
+def clean_dictionary (dictionary)
+  dictionary.each do |word|
+    word.delete("\n")
+  end
+  
+  dictionary.filter! do |word|
+    word.length > 5
+  end
+  dictionary.filter! do |word|
+    word.length < 12
+  end
+end
+
 dictionary = File.new("google-10000-english-no-swears.txt", "r")
 words = dictionary.readlines
-
-words.each do |word|
-  word.delete("\n")
-end
-
-words.filter! do |word|
-  word.length > 5
-end
-words.filter! do |word|
-  word.length < 12
-end
-
-puts words.sample
+clean_dictionary(words)
+puts words.sample #outputs word that is to be used in hangman gameplay
