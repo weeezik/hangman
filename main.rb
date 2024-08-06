9b43407c2499d93684b4a142f46f34e446f6c4b7
weeezik <mkihanya.33@gmail.com>
2024-08-06 13:02:15 -0700

    Create GUI with word-length number of dashes
    To do:
    - need to change how the GUI dashes are represented
    because they cannot easily be referenced for
    modification (when the time inevitably comes when
    a user guesses a correct letter)

--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
-
 def clean_dictionary (dictionary)
   dictionary.each do |word|
     word.delete("\n")
   end
-  
+
   dictionary.filter! do |word|
     word.length > 5
   end
@@ -15,4 +14,20 @@ end
 dictionary = File.new("google-10000-english-no-swears.txt", "r")
 words = dictionary.readlines
 clean_dictionary(words)
-puts words.sample #outputs word that is to be used in hangman gameplay
+secret_word = words.sample.chomp #outputs word that is to be used in hangman gameplay
+
+secret_word.length.times do #change this
+    print "_ "
+end
+puts secret_word
+
+require 'io/console'
+guessed_letter = STDIN.getch.chomp
+
+secret_word.each_char do |char|
+  if char == guessed_letter
+    puts true
+  else
+    puts false
+  end
+end
