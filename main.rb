9a06328a470545bf5934a45eaf71ce4f615b0224
weeezik <mkihanya.33@gmail.com>
2024-08-07 13:44:01 -0700

    Display letters used and player lives left
    To do:
    - draw out game cycle
    - write comparison method fully to check for if
    letter is in the secret word, then replacing the
    display word with the correctly guessed letter(s)
    - OOP/structure code/file management (might help
    to draw out potential classes/objects/methods)

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'io/console'
+
 def clean_dictionary (dictionary)
   dictionary.each do |word|
     word.delete("\n")
@@ -15,39 +17,32 @@ dictionary = File.new("google-10000-english-no-swears.txt", "r")
 words = dictionary.readlines
 clean_dictionary(words)
 secret_word = words.sample.chomp #outputs word that is to be used in hangman gameplay
-p secret_word.chars.join(" ")
+puts secret_word.chars.join(" ")
 
 display_word_array = Array.new(secret_word.length, "_")
-p display_word_array.join(" ")
+puts display_word_array.join(" ")
 
+class Player
+  def initialize (lives = 10)
+    @lives = lives
+  end
+  attr :lives
+end
 
+player = Player.new
+puts "Player lives: #{player.lives}"
 
-require 'io/console'
+letters_used = "Letters used: "
+
+#get player input here
 guessed_letter = STDIN.getch.chomp
 
-secret_word.each_char do |char|
-  if char == guessed_letter
-    guessed_letter.chomp
-  else
-    "_".chomp
-  end
+if secret_word.include?(guessed_letter)
+  #put correctly guessed letter into display word anywhere it appears
+  puts true
+else
+  #add incorrectly guessed letter to letters_used
+  letters_used << guessed_letter + " "
+  puts "Player lives: #{player.lives - 1}"
+  puts letters_used
 end
-
-
-=begin
-secret_hash = {}
-index = 0
-secret_word.each_char do |char|
-  secret_hash[index] = char
-  index+=1 
-end
-puts secret_hash
-
-display_hash = {}
-index = 0
-secret_word.length.times do
-  display_hash[index] = "_"
-  index+=1
-end
-puts display_hash
-=end
