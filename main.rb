a2deeaa78952e692d595b10bf153eb796b6fcf49
weeezik <mkihanya.33@gmail.com>
2024-08-07 12:54:14 -0700

    Store secret word in format that matches display word

--- main.rb
+++ main.rb
@@ -15,19 +15,39 @@ dictionary = File.new("google-10000-english-no-swears.txt", "r")
 words = dictionary.readlines
 clean_dictionary(words)
 secret_word = words.sample.chomp #outputs word that is to be used in hangman gameplay
+p secret_word.chars.join(" ")
 
-secret_word.length.times do #change this
-    print "_ "
-end
-puts secret_word
+display_word_array = Array.new(secret_word.length, "_")
+p display_word_array.join(" ")
+
+
 
 require 'io/console'
 guessed_letter = STDIN.getch.chomp
 
 secret_word.each_char do |char|
   if char == guessed_letter
-    puts true
+    guessed_letter.chomp
   else
-    puts false
+    "_".chomp
   end
 end
+
+
+=begin
+secret_hash = {}
+index = 0
+secret_word.each_char do |char|
+  secret_hash[index] = char
+  index+=1 
+end
+puts secret_hash
+
+display_hash = {}
+index = 0
+secret_word.length.times do
+  display_hash[index] = "_"
+  index+=1
+end
+puts display_hash
+=end
