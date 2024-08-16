b52a37c7fffe04db4bdeddff93e6b58acba1e5ad
weeezik <mkihanya.33@gmail.com>
2024-08-16 11:12:33 -0700

    Fix save/unsave issue
    Inability to save/load then resave accurately
    before variables were reassigned in the
    unserialize method in the serializable module
    
    To do:
    - check if "m" bug is still present
    - allow for multiple games to be saved

--- lib/serializable.rb
+++ lib/serializable.rb
@@ -1,12 +1,22 @@
 module Serializable
   @@serializer = JSON
 
+  # def serialize
+  #   obj = {}
+  #   instance_variables.map do |var|
+  #     obj[var] = instance_variable_get(var)
+  #   end
+  #   @@serializer.dump obj
+  # end
   def serialize
-    obj = {}
-    instance_variables.map do |var|
-      obj[var] = instance_variable_get(var)
-    end
-    @@serializer.dump obj
+    JSON.dump({
+      :player_lives => @player_lives,
+      :letters_used => @letters_used,
+      :secret_word_array => @secret_word_array,
+      :secret_word => @secret_word,
+      :display_word_array => @display_word_array,
+      :display_word => @display_word
+    })
   end
   
   def save_game
@@ -19,11 +29,20 @@ module Serializable
 
   def unserialize (string)
     obj = @@serializer.parse string
-    puts "\n#{obj["@display_word"]}     Lives = #{obj["@player_lives"]}     Letters used: #{obj["@letters_used"]}"
-    self.play(obj["@player_lives"], 
-    obj["@secret_word"], 
-    obj["@display_word"],
-    obj["@secret_word_array"], obj["@letters_used"])
+    
+    #reassign variables
+    self.player_lives = obj["player_lives"]
+    self.letters_used = obj["letters_used"]
+    self.secret_word_array = obj["secret_word_array"]
+    self.secret_word = obj["secret_word"]
+    self.display_word_array = obj["display_word_array"]
+    self.display_word = obj["display_word"]
+
+    puts "\n#{obj["display_word"]}     Lives = #{obj["player_lives"]}     Letters used: #{obj["letters_used"]}"
+    self.play(obj["player_lives"], 
+    obj["secret_word"], 
+    obj["display_word"],
+    obj["secret_word_array"], obj["letters_used"])
   end
 
 end
